18d6259d238412c9aad2959aa01dbc691c1c0eb1
RostSuslov <suslovros@gmail.com>
2024-03-14 21:17:43 +0200

    feat: advantages done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,11 +1,4 @@
-const hamburger = document.querySelector('.hamburger')
-const headerNav = document.querySelector('.header-nav')
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('is-active')
-    headerNav.classList.toggle('show')
-})
-
+$('select').niceSelect();
 
 $('#first-screen-slider').slick({
     dots: true,
@@ -19,24 +12,66 @@ $('#first-screen-slider').slick({
 });
 
 
-$('select').niceSelect();
 
+
+
+function mobileOnlySlider($slidername, $breakpoint) {
+  const slider = $($slidername);
+  const settings = {
+    mobileFirst: true,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+} // Mobile Only Slider
+
+mobileOnlySlider(".slider-advantages", 1024);
+
+
+
+const hamburger = document.querySelector('.hamburger')
+const headerNav = document.querySelector('.header-nav')
 const fileInput = document.querySelector('#calculation-file');
 const fileName = document.querySelector('#file-name')
+const controledField = document.querySelectorAll('[type="checkbox"]')
+const fileField = document.querySelectorAll('[type="file"]')
+const formBtns = document.querySelectorAll('form .col button')
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('is-active')
+    headerNav.classList.toggle('show')
+})
+
 
 fileInput.addEventListener('change', () => {
     fileName.innerHTML = fileInput.files[0].name;
 });
 
 
-const controledField = document.querySelectorAll('[type="checkbox"]')
-const fileField = document.querySelectorAll('[type="file"]')
+const addParentClass = (array, parent, className)=> {
+    array.forEach(item => {
+        item.closest(parent).classList.add(className)
+    })
+}
 
-controledField.forEach(input => {
-    input.closest('.input-wrapper').classList.add('controled-wrapper')
-    input.closest('.col').classList.add('controled-col')
-})
+addParentClass(formBtns, '.col', 'mobile-center')
+addParentClass(controledField, '.input-wrapper', 'controled-wrapper')
+addParentClass(controledField, '.col', 'controled-col')
+addParentClass(fileField, '.input-wrapper', 'file-wrapper')
 
-fileField.forEach(input => {
-  input.closest('.input-wrapper').classList.add('file-wrapper')
-})
+
